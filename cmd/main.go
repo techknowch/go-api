4122f92617ef8101a5d6fd2762dead14507f0bad
Syed Muhammad Ali <techknowasia@proton.me>
2025-04-01 19:30:35 +0500

    Update main.go

--- cmd/main.go
+++ cmd/main.go
@@ -1,31 +1,15 @@
 package main
 
 import (
-	"net/http"
-	"github/gin-gonic/gin"
+    "log"
+    "net/http"
+    "go-api/internal/routes"
 )
 
-type Product struct {
-	ID    string  `json:"id"`
-	Name  string  `json:"name"`
-	Price float64 `json:"price"`
-	Stock int     `json:"stock"`
-}
-
-var products = []Product{
-	{ID: "1", Name: "Product 1", Price: 10.0, Stock: 100},
-	{ID: "2", Name: "Product 2", Price: 20.0, Stock: 200},
-	{ID: "3", Name: "Product 3", Price: 30.0, Stock: 300},
-	{ID: "4", Name: "Product 4", Price: 40.0, Stock: 400},
-	{ID: "5", Name: "Product 5", Price: 50.0, Stock: 500},
-}
-
 func main() {
-	router := gin.Default()
-	router.GET("/products", getProducts)
-	router.GET("/products/:id", getProductByID)
-	router.POST("/products", createProduct)
-	router.PUT("/products/:id", updateProduct)
-	router.DELETE("/products/:id", deleteProduct)
-	router.Run("localhost:8080")
-	}
+    router := routes.SetupRoutes()
+    log.Println("Starting server on :8080")
+    if err := http.ListenAndServe(":8080", router); err != nil {
+        log.Fatalf("Could not start server: %s\n", err)
+    }
+}
