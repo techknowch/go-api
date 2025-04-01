07f9033936270bc17990d828e80f563de24d4a4e
Syed Muhammad Ali <techknowasia@proton.me>
2025-04-01 18:36:09 +0500

    Create handler.go
    define handle get user and handle post product functions

---
+++ internal/handlers/handler.go
@@ -0,0 +1,24 @@
+package handlers
+
+import (
+	"net/http"
+	"encoding/json"
+	"go-api/internal/models"
+)
+
+func HandleGetUser(w http.ResponseWriter, r *http.Request) {
+	products := models.GetProducts()
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(products)
+}
+
+func HandlePostProduct(w http.ResponseWriter, r *http.Request) {
+	var product models.Product
+	if err := json.NewDecoder(r.Body).Decode(&product); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	models.CreateProduct(product)
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(product)
+}
