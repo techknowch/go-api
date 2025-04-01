d78daf85a6e16c082a29ee358940361bd3cc95e6
Syed Muhammad Ali <techknowasia@proton.me>
2025-04-01 19:28:39 +0500

    Create service.go
    GetUsers and CreateProduct method

---
+++ internal/services/service.go
@@ -0,0 +1,24 @@
+package services
+
+import (
+	"errors"
+	"go-api/internal/models"
+)
+
+var users []models.User
+var products []models.Product
+
+func GetUsers() ([]models.User, error) {
+	if len(users) == 0 {
+		return nil, errors.New("no users found")
+	}
+	return users, nil
+}
+
+func CreateProduct(product models.Product) (models.Product, error) {
+	if product.ID == "" || product.Name == "" {
+		return models.Product{}, errors.New("invalid product data")
+	}
+	products = append(products, product)
+	return product, nil
+}
